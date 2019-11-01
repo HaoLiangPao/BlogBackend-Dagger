3cd98933eb39d0fbd844d8833787359f9947aaec
HaoLiangPao <hflsforeverhao@gmail.com>
2019-10-31 23:27:26 -0400

    DaggerMoudule Server-Provider is completed, still working on MongoDB setting up

--- starter_code/src/main/java/ca/utoronto/utm/mcs/App.java
+++ starter_code/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -1,5 +1,6 @@
 package ca.utoronto.utm.mcs;
 
+import com.mongodb.client.MongoClient;
 import java.net.URI;
 import java.net.URISyntaxException;
 import com.sun.net.httpserver.HttpServer;
@@ -10,11 +11,23 @@ import dagger.ObjectGraph;
 public class App implements Runnable
 {
     @Inject HttpServer server;
+    @Inject MongoClient client;
     @Inject Config config;
 
     public void run()
     {
         /* TODO: Add Working Context Here */
+
+        // some endpoints for required functionality
+//        server.createContext("/api/v1/addActor", new addActor(database));
+//        server.createContext("/api/v1/addMovie", new addMovie(database));
+//        server.createContext("/api/v1/addRelationship", new addRelationship(database));
+//        server.createContext("/api/v1/getActor", new getActor(database));
+//        server.createContext("/api/v1/getMovie", new getMovie(database));
+//        server.createContext("/api/v1/hasRelationship", new hasRelationship(database));
+//        server.createContext("/api/v1/computeBaconPath", new computeBaconPath(database));
+//        server.createContext("/api/v1/computeBaconNumber", new computeBaconNumber(database));
+
         server.setExecutor(null);
         server.start();
         System.out.printf("Server started on port %d...\n", config.port);
--- starter_code/src/main/java/ca/utoronto/utm/mcs/DaggerModule.java
+++ starter_code/src/main/java/ca/utoronto/utm/mcs/DaggerModule.java
@@ -1,5 +1,7 @@
 package ca.utoronto.utm.mcs;
 
+import com.mongodb.client.MongoClientFactory;
+import com.mongodb.client.MongoDatabase;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
@@ -9,6 +11,8 @@ import dagger.Module;
 import dagger.Provides;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import javax.inject.Singleton;
+
 
 @Module (injects = {App.class}, library = true) //TODO: Add in any new classes here
 class DaggerModule {
@@ -19,12 +23,14 @@ class DaggerModule {
     }
 
     @Provides MongoClient provideMongoClient() {
-        /* TODO: Fill in this function */
-        return null;
+        // return a MongoClient created from a local class with required Database Name and
+        // Collection name
+        MongoDB clientDB = new MongoDB();
+        return clientDB.getClient();
     }
 
-    @Provides HttpServer provideHttpServer() {
-        /* TODO: Fill in this function */
-        return null;
+    @Provides HttpServer provideHttpServer() throws IOException {
+        // create a HttpServer at ip and port according to config
+        return HttpServer.create(new InetSocketAddress(config.ip, config.port), 0);
     }
 }
