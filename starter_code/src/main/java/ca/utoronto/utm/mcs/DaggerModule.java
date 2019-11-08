01c3c1d9b788cfc0c88bade54c12a2f04bfd1e13
Hao Liang <31187252+HaoLiangPao@users.noreply.github.com>
2019-11-07 20:31:18 -0500

    Revert "Hot fix"

--- starter_code/src/main/java/ca/utoronto/utm/mcs/DaggerModule.java
+++ starter_code/src/main/java/ca/utoronto/utm/mcs/DaggerModule.java
@@ -1,62 +1,30 @@
 package ca.utoronto.utm.mcs;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
+import javax.inject.Inject;
 import dagger.Module;
 import dagger.Provides;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
-
-@Module (injects = {App.class, MongoDB.class, Post.class, postConvertor.class, putPost.class, deletePost.class}, library = true) //TODO: Add in any new classes here
+@Module (injects = {App.class}, library = true) //TODO: Add in any new classes here
 class DaggerModule {
     Config config;
-    Server server;
 
     DaggerModule(Config cfg) {
         config = cfg;
     }
 
-    @Provides MongoDB provideMongoClient() {
-        // return a MongoClient created from a local class with required Database Name and
-        // Collection name
-        return new MongoDB();
-    }
-
-    @Provides putPost providePutPost() {
-        // return a putPost class to handel communication with mongoDB when PUT is used
-        System.out.println("Log: putPost is created");
-        return new putPost();
-    }
-
-    @Provides deletePost providedeletePost() {
-        // return a deletePost class to handel communication with mongoDB when DELETE is used
-        System.out.println("Log: deletePost is created");
-        return new deletePost();
-    }
-
-    @Provides Post providePost() {
-        // return a Post class to store necessary data for a legit post
-        Post post = new Post();
-        System.out.println("Log: Post is created");
-        return post.getPost();
-    }
-
-    @Provides postConvertor provideConvertor(){
-        // return a postConvertor class to convert data from document to Post
-        postConvertor convertor = new postConvertor();
-        System.out.println("Log: postConvertor is created");
-        return convertor.getPostConvertor();
+    @Provides MongoClient provideMongoClient() {
+        /* TODO: Fill in this function */
+        return null;
     }
 
     @Provides HttpServer provideHttpServer() {
-        // create a HttpServer at ip and port according to config
-        try {
-            this.server = new Server(config.ip, config.port);
-        }
-        catch (IOException e){
-            server = null;
-        }
-        System.out.println("Log: server is created");
-        return server.getServer();
+        /* TODO: Fill in this function */
+        return null;
     }
 }
